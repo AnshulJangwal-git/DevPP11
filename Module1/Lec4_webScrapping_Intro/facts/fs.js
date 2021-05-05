44d80c8b5e327a3356e2b483ee890174c464a67b
anshul <anshuljangwal006@gmail.com>
2021-05-05 14:31:08 +0530

    selectors added

--- Module1/Lec4_webScrapping_Intro/facts/fs.js
+++ Module1/Lec4_webScrapping_Intro/facts/fs.js
@@ -17,4 +17,28 @@ let myDocument = cheerio.load(htmlkaData) ;
 let h1kaData = myDocument("h2").text() ;
 // console.log(h1Element) ;
 
-console.log(h1kaData) ;
+// console.log(h1kaData) ;
+
+//selectors..
+// console.log(myDocument("ul p").text()) ;
+
+//a tag
+// console.log(myDocument("a").text()) ;
+
+//To get all a tag inside li
+// console.log(myDocument("ul li a").text()) ;
+
+// //only Direct child..
+// console.log(myDocument("ul>a").text()) ;
+
+
+//classes...
+//dot
+// console.log(myDocument(".inside").text()) ;
+
+// console.log(myDocument(".inside.main").text()) ;
+
+//IDs =>
+//# 
+console.log(myDocument("#main-heading").text()) ;
+
