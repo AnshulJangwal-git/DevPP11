da9d1753894da2d0601d2e0db6767cddbf06c0c2
anshul <anshuljangwal006@gmail.com>
2021-05-24 19:17:43 +0530

    Hackerrank logging in !!

--- Module1/Lec8_Automation/login.js
+++ Module1/Lec8_Automation/login.js
@@ -1,10 +1,19 @@
 const puppeteer = require("puppeteer") ;
 
+const id = "jangwal006@gmail.com" ;
+const pw = "PRANJAl@006" ;
+
+let tab ;
+
 //puppeteer has promisfied functions..
 //by default headless = true 
 
-let browserOpenPromise = puppeteer.launch({ headless : false}) ;
-console.log(browserOpenPromise) ;
+let browserOpenPromise = puppeteer.launch({
+     headless : false,
+     defaultViewport : null ,
+     args : ["--start-maximised"] ,
+}) ;
+// console.log(browserOpenPromise) ;
 //Promise pending..
 
 browserOpenPromise.then(function(browser){
@@ -12,11 +21,20 @@ browserOpenPromise.then(function(browser){
     return browser.pages() ;
 })
 .then(function(pages){
-    let tab = pages[0] ;
-    return tab.goto("https://www.youtube.com") ;
+    tab = pages[0] ;
+    return tab.goto("https://www.hackerrank.com/auth/login") ;
     
 })
 .then(function(){
-    console.log("On google homepage!! ") ;
-    console.log("On youtube!! ") ;
+    return tab.type("#input-1", id) ;
+})
+.then(function(){
+    return tab.type("#input-2", pw) ;
+})
+.then(function(){
+    return tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled") ;
+
+})
+.then(function(){
+    console.log("logged in !!") ;
 })
