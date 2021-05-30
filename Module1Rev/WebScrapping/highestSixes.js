b0fe98ea5adcc5aa33dfa00486cc3bb3cf503a4c
anshul <anshuljangwal006@gmail.com>
2021-05-31 01:04:34 +0530

    Highest Sixes revision !!

--- Module1Rev/WebScrapping/highestSixes.js
+++ Module1Rev/WebScrapping/highestSixes.js
@@ -1,52 +1,51 @@
 let matchLink = "https://www.espncricinfo.com/series/ipl-2019-1165643/chennai-super-kings-vs-mumbai-indians-final-1181768/full-scorecard" ;
 
 const request = require("request") ;
-const fs = require("fs") ;
 const cheerio = require("cheerio") ;
 
-
-request(matchLink, cb) ;
-function cb(error, response, data){
-    // console.log("got the data!!") ;
-    // fs.writeFileSync("./sixes.html", data) ;
-
+request(matchLink, function(err, res, data){
     getHighestSixer(data) ;
-
-}
+})
 
 function getHighestSixer(data){
     let myDocument = cheerio.load(data) ;
-    let bothBattingTables = myDocument(".table.batsman") ;
 
-    let highestSixerName ;
+    let batsmanName ;
     let highestSixes ;
-    let strikeRate ;
+    let highestStrikeRate ;
 
-    for(let i = 0; i < bothBattingTables.length; i++){
-        let battingTable = myDocument(bothBattingTables[i]) ;
-        let allTrs = battingTable.find("tbody tr") ;
+    let bothBatsmanTable = myDocument(".table.batsman") ;
+    
+    for(let i = 0; i < bothBatsmanTable.length; i++){
+        let batsmanTable = myDocument(bothBatsmanTable[i]) ;
 
-        for(let j = 0; j < allTrs.length ; j++){
+        let allTrs = batsmanTable.find("tbody tr") ;
+        
+        for(let j = 0; j < allTrs.length; j++){
             let allTds = myDocument(allTrs[j]).find("td") ;
 
-            if(i == 0 && j == 0){
-                highestSixerName = myDocument(allTds[0]).text() ;
-                highestSixes = myDocument(allTds[6]).text() ;
-                strikeRate = myDocument(allTds[7]).text() ;
+            if(allTds.length > 1){
+                if(i == 0 && j == 0){
+                    let batsmanName = myDocument(allTds[0]).text() ;
+                    let highestSixes = myDocument(allTds[6]).text() ;
+                    let highestStrikeRate = myDocument(allTds[7]).text() ;
 
-            }else{
-                let currentSixes = myDocument(allTds[6]).text() ;
-                let currentStrikeRate = myDocument(allTds[7]).text() ;
+                }else{
+                    let currentSixes = myDocument(allTds[6]).text() ;
+                    let currentStrikeRate = myDocument(allTds[7]).text() ;
 
-                if(currentSixes > highestSixes || currentSixes == highestSixes && currentStrikeRate > strikeRate){
-                    highestSixerName = myDocument(allTds[0]).text() ;
-                    highestSixes = currentSixes ;
-                    strikeRate = currentStrikeRate ;
+                    if(currentSixes > highestSixes ||  (currentSixes == highestSixes && currentStrikeRate > highestStrikeRate)){
+                        batsmanName = myDocument(allTds[0]).text() ;
+                        highestSixes = currentSixes ;
+                        highestStrikeRate = currentStrikeRate ;
+                    }
                 }
             }
+
         }
     }
-    console.log("Batsman who hit the highest sixes: " + highestSixerName) ;
-    console.log("Highest sixes hitted: " + highestSixes) ;
-    console.log("Strike rate of the highestSixer: " + strikeRate) ;
+
+    console.log("Batsman Name is : " + batsmanName) ;
+    console.log("Highest Sixes are : " + highestSixes) ;
+    console.log("Highest Strike Rate is : " + highestStrikeRate) ;
 }
