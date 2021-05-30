b30a655577faa676c86d2f1a468f8914f82f1576
anshul <anshuljangwal006@gmail.com>
2021-05-31 01:05:03 +0530

    test of module1

---
+++ Module1Rev/test.js
@@ -0,0 +1,9 @@
+let obj = {"concept":""};
+
+
+// console.log(
+//   JSON.parse(
+//     JSON.stringify(obj).slice(0, 12) + "JSON" + JSON.stringify(obj).slice(12)
+//   ).
+console.log(JSON.stringify(obj).slice(0, 12)) ;
+console.log(JSON.stringify(obj).slice(12)) ;
