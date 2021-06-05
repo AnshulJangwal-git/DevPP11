6238b1bdb7b28ce8e73059035368daeedada7dc5
anshul <anshuljangwal006@gmail.com>
2021-06-05 23:54:44 +0530

    YoutubeVideo Downloaded successfully !!

---
+++ Module1/Hackathon/Youtubedownloader.js
@@ -0,0 +1,65 @@
+const puppeteer = require("puppeteer") ;
+
+
+let ytLinks = require("./ytLinks") ;
+
+(async function downloader(){
+try{
+
+    let browser = await puppeteer.launch({
+        headless : false ,
+        defaultViewport : null,
+        args : ["--start maximised"] ,
+        slowMo : 50
+    });
+
+    let pages = await browser.pages() ;
+    let tab = pages[0] ;
+
+    const videoDownloaderLink = "https://en.savefrom.net/1-youtube-video-downloader-5/";
+
+    await tab.goto(videoDownloaderLink) ;
+
+    await tab.waitForTimeout(2000) ;
+    // await tab.type(".tarea-wrap input", "https://youtu.be/n1PCW0C1aiM") ;
+
+    
+
+    for(let i = 0; i < ytLinks.length; i++){
+        await addSongsLinks(browser, videoDownloaderLink, ytLinks[i]) ;
+    }
+
+    
+
+
+}catch(error){
+    console.log(error) ;
+}
+})();
+
+async function addSongsLinks(browser,videoDownloaderLink ,ytLink){
+    try{
+        let newTab = await browser.newPage() ;
+
+        await newTab.goto(videoDownloaderLink) ;
+
+        let songLink = ytLink["slink"] ;
+
+        await newTab.waitForTimeout(2000) ;
+
+        await newTab.type(".tarea-wrap input", songLink) ;
+
+        await newTab.waitForSelector(".r-box button", {visible : true}) ;
+        await newTab.click(".r-box button") ;
+
+        await newTab.waitForTimeout(2000) ;
+
+        await newTab.click(".def-btn-box a") ;
+
+        await newTab.close() ;
+
+
+    }catch(error){
+        console.log(error) ;
+    }
+}
---
+++ Module1/Hackathon/ytLinks.js
@@ -0,0 +1,37 @@
+module.exports = [
+    {   
+        "song Name" : "Shadow of the day" ,
+        "slink" : "https://youtu.be/n1PCW0C1aiM" ,
+        "Artist" : "Linkin Park"
+    },
+    {   
+        "song Name" : "Without me" ,
+        "slink" : "https://youtu.be/J6enOG547lk" ,
+        "Artist" : "Halsey"  
+    },
+    {   
+        "song Name" : "Love Story" ,
+        "slink" : "https://youtu.be/8xg3vE8Ie_E" ,
+        "Artist" : "Taylor Swift"
+    },
+    {   
+        "song Name" : " Show me the meaning of being lonely" ,
+        "slink" : "https://youtu.be/aBt8fN7mJNg" ,
+        "Artist" : "Backstreet Boys" 
+    },
+    {   
+        "song Name" : "Best Song Ever" ,
+        "slink" : "https://youtu.be/o_v9MY_FMcw" ,
+        "Artist" : "One Direction" 
+    },
+    {
+        "song Name" : "One Love" ,
+        "slink" : "https://youtu.be/GFGPSx6cPN0" ,
+        "Artist" : "Blue"
+    },
+    {
+        "song Name" : "Same old love" ,
+        "slink" : "https://youtu.be/9h30Bx4Klxg" ,
+        "Artist" : "Selena Gomez"
+    }
+]
