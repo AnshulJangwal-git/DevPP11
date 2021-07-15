47bdaa28660bb50695c4d65b417929d2f63d1350
anshul <anshuljangwal006@gmail.com>
2021-07-15 14:30:07 +0530

    solve formula done !!

--- Module2/Excel/init.js
+++ Module2/Excel/init.js
@@ -2,8 +2,7 @@ let cellsContainer = document.querySelector(".cells");
 let db ;
 
 function initCells() {
-    let cellsContent = "";
-    cellsContent += `<div class = "top-left-cell"></div>`;
+    let cellsContent = '<div class="top-left-cell"></div>';
 
     cellsContent += '<div class="top-row">';
     for (let j = 0; j < 26; j++) {
@@ -21,35 +20,33 @@ function initCells() {
     for (let i = 0; i < 100; i++) {
         cellsContent += '<div class="row">';
         for (let j = 0; j < 26; j++) {
-            cellsContent += `<div class="cell" contenteditable="true" rowid = "${i}" colid = "${j}"></div>`;
+            cellsContent += `<div class="cell" contenteditable="true" rowid="${i}" colid="${j}"></div>` ;
         }
-        cellsContent += "</div>";
+        cellsContent += "</div>" ;
     }
     cellsContent += "</div";
     cellsContainer.innerHTML = cellsContent;
 }
 
-function initDB(){
-    db = [] ;
-
-    for(let i = 0; i < 100; i++){
-        let row = [] ;
-        for(let j = 0; j < 26; j++){
-            // i = 1
-            // j = 1 => B2
-            let cellName = String.fromCharCode(65 + j) + (i + 1) ;
+function initDB() {
+    db = [];
+    for (let i = 0; i < 100; i++) {
+        let row = [];
+        for (let j = 0; j < 26; j++) {
+            // i=1
+            // j=1   =>   B2
+            let cellName = String.fromCharCode(65 + j) + (i + 1);
             let cellObject = {
-                Name : cellName, 
-                value : "",
-                formula : "",
-            } ;
-            row.push(cellObject) ;
+                name: cellName,
+                value: "",
+                formula: "",
+            };
+            row.push(cellObject);
         }
-        db.push(row) ;
-        console.log(db) ;
+        db.push(row);
     }
+    console.log(db);
 }
 
-
-initCells();
+initCells() ;
 initDB() ;
--- Module2/Excel/script.js
+++ Module2/Excel/script.js
@@ -1,33 +1,91 @@
-let topLeftcell = document.querySelector(".top-left-cell") ;
-let topRow = document.querySelector(".top-row") ;
-let leftCol = document.querySelector(".left-col") ;
+let topLeftCell = document.querySelector(".top-left-cell");
+let topRow = document.querySelector(".top-row");
+let leftCol = document.querySelector(".left-col");
 
+let address = document.querySelector("#address");
+let formulaInput = document.querySelector("#formula");
+let allCells = document.querySelectorAll(".cell");
+let lastSelectedCell;
 
-cellsContainer.addEventListener("scroll", function(e){
-    let topOffSet = e.target.scrollTop ;
-    let leftOffSet = e.target.scrollLeft ;
+cellsContainer.addEventListener("scroll", function (e) {
+    let topOffset = e.target.scrollTop;
+    let leftOffset = e.target.scrollLeft;
 
-    topRow.style.top = topOffSet + "px" ;
-    topLeftcell.style.top = topOffSet + "px" ;
-    topLeftcell.style.left = leftOffSet + "px" ;
-    leftCol.style.left = leftOffSet + "px" ;
+    topRow.style.top = topOffset + "px";
+    topLeftCell.style.top = topOffset + "px";
+    topLeftCell.style.left = leftOffset + "px";
+    leftCol.style.left = leftOffset + "px";
+});
 
-}) ;
+formulaInput.addEventListener("blur", function (e) {
+    let formula = e.target.value;
+    if (formula) {
+        let calculatedValue = solveFormula(formula);
+        // UI Update
+        lastSelectedCell.textContent = calculatedValue;
+        // DB Update
+        let cellObject = getCellObjectFromElement(lastSelectedCell);
+        cellObject.value = calculatedValue;
+        cellObject.formula = formula;
+    }
+});
 
-let allCells = document.querySelectorAll(".cell") ;
+for (let i = 0; i < allCells.length; i++) {
+    allCells[i].addEventListener("click", function (e) {
+        let cellObject = getCellObjectFromElement(e.target);
+        address.value = cellObject.name ;
+        formulaInput.value = cellObject.formula ;
+    });
 
-for(let i = 0; i < allCells.length; i++){
-    allCells[i].addEventListener("blur", function(e){
-        // console.log(e) ;
+    allCells[i].addEventListener("blur", function (e) {
+        // logic to save this value in db
+        lastSelectedCell = e.target;
 
-        //To save the values in db..
-        let cellValueFromUI = e.target.textContent ;
-        let rowId = e.target.getAttribute("rowid") ;
-        let colId = e.target.getAttribute("colid") ;
+        let cellValueFromUI = e.target.textContent;
+        // cellObject ki value update !!
+        if (cellValueFromUI) {
+            let cellObject = getCellObjectFromElement(e.target);
+            cellObject.value = cellValueFromUI;
 
-        let cellObject = db[rowId][colId] ;
-        cellObject.value = cellValueFromUI ;
-
-
-    })
+        }
+    }) ;
 }
+
+function solveFormula(formula) {
+    // tip : implement infix evalutaion
+    // ( A1 + A2 ) => ( 10 + 20 );
+    let formulaComps = formula.split(" ");
+    // ["(" , "A1" , "+" , "A2" , ")"];
+    // find valid component
+    for (let i = 0; i < formulaComps.length; i++) {
+        let fComp = formulaComps[i];
+        if (
+            (fComp[0] >= "A" && fComp[0] <= "Z") ||
+            (fComp[0] >= "a" && fComp <= "z")
+        ) {
+            // A1 || A2
+            // fComp = A1
+            let cellObject = getCellObjectFromName(fComp);
+            let value = cellObject.value;
+            formula = formula.replace(fComp, value);
+        }
+    }
+    // ( 10 + 20 ) => infix evaluation
+    let calculatedValue = eval(formula);
+    return calculatedValue;
+
+}
+
+function getCellObjectFromElement(element) {
+    let rowId = element.getAttribute("rowid");
+    let colId = element.getAttribute("colid");
+    return db[rowId][colId];
+}
+
+function getCellObjectFromName(name) {
+    // A100
+    let colId = name.charCodeAt(0) - 65;
+    let rowId = Number(name.substring(1)) - 1;
+    return db[rowId][colId];
+}
+
