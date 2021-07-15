f953cd78cd4b32a05e79531cf6987b73e4ce35e9
anshul <anshuljangwal006@gmail.com>
2021-07-15 05:46:10 +0530

    value added to db using blur event !

--- Module2/Excel/script.js
+++ Module2/Excel/script.js
@@ -13,3 +13,21 @@ cellsContainer.addEventListener("scroll", function(e){
     leftCol.style.left = leftOffSet + "px" ;
 
 }) ;
+
+let allCells = document.querySelectorAll(".cell") ;
+
+for(let i = 0; i < allCells.length; i++){
+    allCells[i].addEventListener("blur", function(e){
+        // console.log(e) ;
+
+        //To save the values in db..
+        let cellValueFromUI = e.target.textContent ;
+        let rowId = e.target.getAttribute("rowid") ;
+        let colId = e.target.getAttribute("colid") ;
+
+        let cellObject = db[rowId][colId] ;
+        cellObject.value = cellValueFromUI ;
+
+
+    })
+}
